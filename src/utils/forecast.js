cdfb234edbf6f9cef8403fc1c738f149718268dd
CJ Roberts <cjroberts2013.gmail.com>
2020-01-02 21:59:34 -0600

    Added the temperature high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,8 +18,12 @@ const forecast = (latitude, longitude, callback) => {
 				undefined,
 				body.daily.data[0].summary +
 					" It is currently " +
-					body.currently.temperature +
-					" degrees out. There is a " +
+					Math.floor(body.currently.temperature) +
+					" degrees out. The high today is " +
+					Math.floor(body.daily.data[0].temperatureMax) +
+					" With a low of " +
+					Math.floor(body.daily.data[0].temperatureLow) +
+					". There is a " +
 					Math.floor(body.currently.precipProbability * 100) +
 					"% chance of rain. The current wind speed is " +
 					body.currently.windSpeed +
